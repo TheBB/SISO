4e14a23595aa684edd76a2d91fc7fe841c6c8fc6
Eivind Fonn <evfonn@gmail.com>
2020-03-31 14:29:55 +0200

    Add LR support and LR volume support

---
+++ ifem_to_vt/reader/lr.py
@@ -0,0 +1,42 @@
+import lrspline
+
+from .hdf5 import Log, GeometryManager
+
+
+class SimpleBasis:
+
+    def __init__(self, patches):
+        self.patches = patches
+        self.name = 'Geometry'
+
+    @property
+    def npatches(self):
+        return len(self.patches)
+
+    def update_at(self, stepid):
+        return stepid == 0
+
+    def patch_at(self, stepid, patchid):
+        return self.patches[patchid]
+
+
+class Reader:
+
+    def __init__(self, filename, **kwargs):
+        self.basis = None
+        self.filename = filename
+
+    def __enter__(self):
+        with open(self.filename, 'rb') as f:
+            patches = lrspline.LRSplineObject.read_many(f)
+        self.basis = SimpleBasis(patches)
+        return self
+
+    def __exit__(self, type_, value, backtrace):
+        pass
+
+    def write(self, w):
+        w.add_step()
+        geometry = GeometryManager(self.basis, self)
+        geometry.update(w, 0)
+        w.finalize_step()
