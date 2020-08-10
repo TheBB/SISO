b54675ec12e84bb9697dda70eca261433a7eb580
Eivind Fonn <evfonn@gmail.com>
2020-08-10 13:21:12 +0200

    Simplify LR/G2 readers

--- ifem_to_vt/reader/g2.py
+++ ifem_to_vt/reader/g2.py
@@ -2,55 +2,42 @@ from pathlib import Path
 
 from splipy.io import G2
 
+from typing import List
+
 from .. import config
 from ..geometry import SplinePatch
-from .hdf5 import GeometryManager
 from .reader import Reader
-
-
-class SimpleBasis:
-
-    def __init__(self, patches):
-        self.patches = patches
-        self.name = 'Geometry'
-
-    @property
-    def npatches(self):
-        return len(self.patches)
-
-    def update_at(self, stepid):
-        return stepid == 0
-
-    def patch_at(self, stepid, patchid):
-        return self.patches[patchid]
 
 
 class G2Reader(Reader):
 
     reader_name = "GoTools"
 
+    filename: Path
+    patches: List[SplinePatch]
+
     @classmethod
     def applicable(cls, filename: Path) -> bool:
         return filename.suffix == '.g2'
 
-    def __init__(self, filename):
+    def __init__(self, filename: Path):
         self.filename = filename
         config.require(multiple_timesteps=False)
 
     def __enter__(self):
         with G2(self.filename) as g2:
-            patches = [
+            self.patches = [
                 SplinePatch((i,), patch) for i, patch
                 in enumerate(g2.read())
             ]
-        self.basis = SimpleBasis(patches)
         return self
 
-    def __exit__(self, type_, value, backtrace):
+    def __exit__(self, *args):
         pass
 
     def write(self, w):
         w.add_step(time=0.0)
-        geometry = GeometryManager(self.basis)
-        geometry.update(w, 0)
+        for patch in self.patches:
+            w.update_geometry(patch)
+        w.finalize_geometry()
         w.finalize_step()
--- ifem_to_vt/reader/lr.py
+++ ifem_to_vt/reader/lr.py
@@ -2,55 +2,42 @@ from pathlib import Path
 
 import lrspline
 
+from typing import List
+
 from .. import config
 from ..geometry import LRPatch
-from .hdf5 import GeometryManager
 from .reader import Reader
-
-
-class SimpleBasis:
-
-    def __init__(self, patches):
-        self.patches = patches
-        self.name = 'Geometry'
-
-    @property
-    def npatches(self):
-        return len(self.patches)
-
-    def update_at(self, stepid):
-        return stepid == 0
-
-    def patch_at(self, stepid, patchid):
-        return self.patches[patchid]
 
 
 class LRReader(Reader):
 
     reader_name = "LRSplines"
 
+    filename: Path
+    patches: List[LRPatch]
+
     @classmethod
     def applicable(cls, filename: Path) -> bool:
         return filename.suffix == '.lr'
 
-    def __init__(self, filename):
+    def __init__(self, filename: Path):
         self.filename = filename
         config.require(multiple_timesteps=False)
 
     def __enter__(self):
         with open(self.filename, 'rb') as f:
-            patches = [
+            self.patches = [
                 LRPatch((i,), patch) for i, patch
                 in enumerate(lrspline.LRSplineObject.read_many(f))
             ]
-        self.basis = SimpleBasis(patches)
         return self
 
-    def __exit__(self, type_, value, backtrace):
+    def __exit__(self, *args):
         pass
 
     def write(self, w):
         w.add_step(time=0.0)
-        geometry = GeometryManager(self.basis)
-        geometry.update(w, 0)
+        for patch in self.patches:
+            w.update_geometry(patch)
+        w.finalize_geometry()
         w.finalize_step()
