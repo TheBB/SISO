9e7ff36dfda5081d06c3113b7efe833f238184f4
Eivind Fonn <evfonn@gmail.com>
2020-08-10 13:36:04 +0200

    Rename {hdf5,res}.py to {ifem,simra}.py

--- ifem_to_vt/reader/__init__.py
+++ ifem_to_vt/reader/__init__.py
@@ -2,8 +2,6 @@ import importlib
 from os.path import splitext
 
 from .reader import Reader
-from .hdf5 import IFEMReader, IFEMEigenReader
+from .ifem import IFEMReader, IFEMEigenReader
 from .puregeometry import G2Reader, LRReader
-# from .g2 import G2Reader
-# from .lr import LRReader
-from .res import SIMRAReader
+from .simra import SIMRAReader
