53c279faaa79cd2ca9d3be44c7299a704cd8315d
Eivind Fonn <evfonn@gmail.com>
2020-08-19 12:08:12 +0200

    Bump version: 2.0.0 → 2.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(Path(__file__).parent / 'README.rst') as f:
 
 setup(
     name='IFEM-to-VT',
-    version='2.0.0',
+    version='2.0.1',
     description='Convert between different mesh data formats',
     long_description_content_type='text/x-rst',
     long_description=desc,
