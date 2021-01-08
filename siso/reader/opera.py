3f6e603aeed3e908e4e04870c09ac385ae5d3991
Eivind Fonn <evfonn@gmail.com>
2021-01-08 11:41:11 +0100

    Opera: Add volumetric reader

--- siso/reader/opera.py
+++ siso/reader/opera.py
@@ -1,14 +1,16 @@
 from datetime import datetime
 from itertools import count
+from operator import itemgetter
 from pathlib import Path
 
 from typing import Iterable, Tuple, Union, Dict, List
 
 import h5py
 import numpy as np
+import treelog as log
 
 from .reader import Reader
-from ..fields import Field, SimpleField, FieldPatches, Geometry
+from ..fields import Field, SimpleField, FieldPatches, Geometry, PatchData, FieldData
 from ..geometry import StructuredTopology, Patch, Quad
 from ..typing import StepData
 from ..coords import UTM
@@ -67,6 +69,53 @@ class OperaDataset(OperaGroup):
         for subgrp in interesting_groups(self.grp):
             yield OperaData(subgrp)
 
+    def patch(self, coords: UTM) -> Tuple[PatchData, FieldData]:
+        # Convert center
+        c_lon, c_lat = self.where['lon'], self.where['lat']
+        center = np.array(lonlat_to_utm(c_lon, c_lat, coords.zone_number, coords.zone_letter))
+
+        # Convert a point to the north, to find zero azimuth
+        north = np.array(lonlat_to_utm(c_lon, c_lat + 1e-3, coords.zone_number, coords.zone_letter)) - center
+        north /= np.linalg.norm(north)
+        zero_az = np.arctan2(north[1], north[0])
+
+        anglesync = self.how.get('anglesync', b'azimuth')
+        if anglesync == b'azimuth':
+            elangle = self.where['elangle']
+
+            # Compute radial and azimuthal space
+            h_rscale = self.where['rscale'] * np.cos(np.deg2rad(elangle))
+            rad = self.where['rstart'] + np.arange(self.where['nbins'] + 1) * h_rscale
+            az = zero_az - np.deg2rad(self.how['startazA'])
+            az = np.append(az, [az[0]])
+
+            # Convert to cartesian coordinates
+            rad, az = np.meshgrid(rad, az)
+            x = rad * np.cos(az) + center[0]
+            y = rad * np.sin(az) + center[1]
+
+            # Compute altitude
+            z = rad * np.sin(np.deg2rad(elangle)) + self.where['height']
+
+        elif anglesync == b'elevation':
+            # Compute radial and elevation space
+            rad = self.where['rstart'] + np.arange(self.where['nbins'] + 1) * self.where['rscale']
+            start_elev = self.how['startelA']
+            stop_elev = self.how['stopelA']
+            elev = np.deg2rad((np.append(start_elev, [stop_elev[-1]]) + np.append([start_elev[0]], stop_elev)) / 2)
+
+            # Convert to cartesian coordinates
+            rot = zero_az - np.deg2rad(self.where['azangle'])
+            rad, elev = np.meshgrid(rad, elev)
+            x = rad * np.cos(elev)
+            y = x * np.sin(rot)
+            x *= np.cos(rot)
+            z = rad * np.sin(elev) + self.where['height']
+
+        topo = StructuredTopology((self.where['nrays'], self.where['nbins']), celltype=Quad())
+        nodes = np.vstack([x.flatten(), y.flatten(), z.flatten()]).T
+        return Patch(('geometry',), topo), nodes
+
 
 class OperaField(SimpleField):
 
@@ -79,8 +128,6 @@ class OperaField(SimpleField):
     def __init__(self, name: str, data: List[OperaData]):
         self.name = name
         self.data = data
-        self.ncomps = 1
-        self.cells = True
 
     def patches(self, stepid: int, force: bool = False, **_) -> FieldPatches:
         data = self.data[stepid]
@@ -105,52 +152,52 @@ class OperaGeometryField(SimpleField):
 
     def patches(self, stepid: int, force: bool = False, **_) -> FieldPatches:
         dataset = OperaDataset(self.dataset.grp.parent[f'dataset{stepid+1}'])
-        elangle = dataset.where.get('elangle', 0.0)
+        yield dataset.patch(self.coords)
 
-        # Convert center
-        c_lon, c_lat = dataset.where['lon'], dataset.where['lat']
-        center = np.array(lonlat_to_utm(c_lon, c_lat, self.coords.zone_number, self.coords.zone_letter))
 
-        # Convert a point to the north, to find zero azimuth
-        north = np.array(lonlat_to_utm(c_lon, c_lat + 1e-3, self.coords.zone_number, self.coords.zone_letter)) - center
-        north /= np.linalg.norm(north)
-        zero_az = np.arctan2(north[1], north[0])
+class OperaVolumeGeometryField(SimpleField):
 
-        if dataset.how['anglesync'] == b'azimuth':
-            elangle = dataset.where['elangle']
+    datasets: List[OperaDataset]
 
-            # Compute radial and azimuthal space
-            h_rscale = dataset.where['rscale'] * np.cos(np.deg2rad(elangle))
-            rad = dataset.where['rstart'] + np.arange(dataset.where['nbins'] + 1) * h_rscale
-            az = zero_az - np.deg2rad(dataset.how['startazA'])
-            az = np.append(az, [az[0]])
+    name = 'Geometry'
+    ncomps = 3
+    cells = False
+    decompose = False
 
-            # Convert to cartesian coordinates
-            rad, az = np.meshgrid(rad, az)
-            x = rad * np.cos(az) + center[0]
-            y = rad * np.sin(az) + center[1]
+    def __init__(self, datasets: List[OperaDataset]):
+        assert all(dataset.what['product'] == b'SCAN' for dataset in datasets)
+        self.datasets = datasets
+        self.fieldtype = Geometry(UTM.optimal(datasets[0].where['lon'], datasets[0].where['lat']))
 
-            # Compute altitude
-            z = rad * np.sin(np.deg2rad(elangle)) + dataset.where['height']
+    def patches(self, stepid: int, force: bool = False, **_) -> FieldPatches:
+        patches, nodes = zip(*(dataset.patch(self.coords) for dataset in self.datasets))
+        topos = [patch.topology for patch in patches]
+        newtopo = StructuredTopology.stack(*topos)
+        newnodes = np.stack([n.reshape(*topos[0].nodeshape, -1) for n in nodes], axis=-2).reshape(-1, nodes[0].shape[-1])
+        yield Patch(('geometry',), newtopo), newnodes
+        # yield Patch(('geometry',), topos[0]), nodes[0]
 
-        elif dataset.how['anglesync'] == b'elevation':
-            # Compute radial and elevation space
-            rad = dataset.where['rstart'] + np.arange(dataset.where['nbins'] + 1) * dataset.where['rscale']
-            start_elev = dataset.how['startelA']
-            stop_elev = dataset.how['stopelA']
-            elev = np.deg2rad((np.append(start_elev, [stop_elev[-1]]) + np.append([start_elev[0]], stop_elev)) / 2)
 
-            # Convert to cartesian coordinates
-            rot = zero_az - np.deg2rad(dataset.where['azangle'])
-            rad, elev = np.meshgrid(rad, elev)
-            x = rad * np.cos(elev)
-            y = x * np.sin(rot)
-            x *= np.cos(rot)
-            z = rad * np.sin(elev) + dataset.where['height']
+class OperaVolumeField(SimpleField):
 
-        topo = StructuredTopology((dataset.where['nrays'], dataset.where['nbins']), celltype=Quad())
-        nodes = np.vstack([x.flatten(), y.flatten(), z.flatten()]).T
-        yield Patch(('geometry',), topo), nodes
+    data: List[OperaData]
+
+    ncomps = 1
+    cells = True
+    decompose = False
+
+    def __init__(self, name: str, data: List[OperaData]):
+        self.name = name
+        self.data = data
+
+    def patches(self, stepid: int, force: bool = False, **_) -> FieldPatches:
+        datasets = [OperaDataset(data.grp.parent) for data in self.data]
+        patch, _ = next(OperaVolumeGeometryField(datasets).patches(stepid, force))
+
+        datas = [d.data() for d in self.data]
+        datas = [(a+b)/2 for a, b in zip(datas[:-1], datas[1:])]
+        data = np.stack(datas, axis=-1).reshape(-1, 1)
+        yield patch, data
 
 
 class OperaReader(Reader):
@@ -180,7 +227,6 @@ class OperaReader(Reader):
 
     def __enter__(self):
         self.h5 = h5py.File(self.filename, 'r').__enter__()
-        print(dict(self.h5['what'].attrs))
         return self
 
     def __exit__(self, *args):
@@ -233,3 +279,30 @@ class OperaElevReader(OperaReader):
 
     object_type = 'ELEV'
     reader_name = "EUMETNET-OPERA-ELEV"
+
+
+class OperaPvolReader(OperaReader):
+
+    object_type = 'PVOL'
+    reader_name = "EUMETNET-OPERA-PVOL"
+
+    def steps(self) -> Iterable[Tuple[int, StepData]]:
+        """Iterate over all steps with associated data."""
+        yield (0, {'time': 0.0})
+
+    def fields(self) -> Iterable[Field]:
+        """Iterate over all fields."""
+
+        datasets = {dataset.where['elangle']: dataset for dataset in self.datasets('SCAN')}
+        datasets_sorted = list(map(itemgetter(1), sorted(datasets.items(), key=itemgetter(0))))
+
+        field_data = {}
+        for dataset in datasets_sorted:
+            for data in dataset.data():
+                name = data.what['quantity'].decode()
+                field_data.setdefault(name, []).append(data)
+
+        for name, datas in field_data.items():
+            yield OperaVolumeField(name, datas)
+
+        yield OperaVolumeGeometryField(datasets_sorted)
