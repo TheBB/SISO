2eb090138eef9b2432ebd7efb3cad173fcf60171
Eivind Fonn <evfonn@gmail.com>
2021-08-06 11:25:36 +0200

    Bump version: 3.0.0 → 3.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(Path(__file__).parent / 'README.rst') as f:
 
 setup(
     name='SISO',
-    version='3.0.0',
+    version='3.0.1',
     description='Convert between different mesh data formats',
     long_description_content_type='text/x-rst',
     long_description=desc,
