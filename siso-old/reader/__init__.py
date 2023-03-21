168a43dab9d28c85b9a3130558943e00be323250
Eivind Fonn <evfonn@gmail.com>
2023-03-21 10:56:21 +0100

    Remove old code

--- siso-old/reader/__init__.py
+++
@@ -1,6 +0,0 @@
-from .reader import Reader
-from .ifem import IFEMReader, IFEMEigenReader
-from .opera import OperaScanReader, OperaElevReader
-from .puregeometry import G2Reader, LRReader
-from .simra import SIMRAContinuationReader
-from .wrf import WRFReader
--- siso-old/writer/__init__.py
+++
@@ -1,5 +0,0 @@
-from .writer import Writer
-from .vtk import VTKLegacyWriter, VTUWriter, VTSWriter, PVDWriter
-from .vtf import VTFWriter
-from .nc import NetCDFCFWriter
-from .simra import SIMRAWriter
