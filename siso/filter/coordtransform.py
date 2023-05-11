e4095e62070e22e2ad4ce3ab0d218bd7ac836d41
Eivind Fonn <evfonn@gmail.com>
2023-05-11 09:35:00 +0200

    Simpler coordinate transform interface

--- siso/filter/coordtransform.py
+++ siso/filter/coordtransform.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple
+from typing import Dict, Generic, Tuple
 
 from numpy import floating
 
@@ -7,6 +7,30 @@ from ..api import B, F, S, T, Z
 from ..coord import ConversionPath, convert_coords, convert_vectors
 from ..util import FieldData
 from .passthrough import PassthroughAll
+
+
+class CoordTransformCache(Generic[Z]):
+    path: ConversionPath
+
+    # Conversion of vector fields requires (in general) knowledge of the
+    # coordinates of the points those vector fields are defined on. We store
+    # them in this dict, mapping coordinate system name and zone to nodes.
+    cache: Dict[Tuple[str, Z], FieldData[floating]]
+
+    def __init__(self, path: ConversionPath) -> None:
+        self.path = path
+        self.cache = {}
+
+    def convert_geometry(self, data: FieldData[floating], zone: Z) -> FieldData[floating]:
+        for a, b in util.pairwise(self.path):
+            self.cache[a.name, zone] = data
+            data = convert_coords(a, b, data)
+        return data
+
+    def convert_vectors(self, data: FieldData[floating], zone: Z) -> FieldData[floating]:
+        for a, b in util.pairwise(self.path):
+            data = convert_vectors(a, b, data, self.cache[a.name, zone])
+        return data
 
 
 class CoordTransform(PassthroughAll[B, F, S, T, Z]):
@@ -21,34 +45,22 @@ class CoordTransform(PassthroughAll[B, F, S, T, Z]):
         `siso.coord.conversion_path` or `siso.coord.optimal_system`.
     """
 
-    path: ConversionPath
-
-    # Conversion of vector fields requires (in general) knowledge of the
-    # coordinates of the points those vector fields are defined on. We store
-    # them in this dict, mapping coordinate system name and zone to nodes.
-    cache: Dict[Tuple[str, Z], FieldData[floating]]
+    cache: CoordTransformCache[Z]
 
     def __init__(self, source: api.Source[B, F, S, T, Z], path: ConversionPath):
         super().__init__(source)
-        self.path = path
-        self.cache = {}
+        self.cache = CoordTransformCache(path)
 
     def field_data(self, timestep: S, field: F, zone: Z) -> FieldData[floating]:
-        indata = self.source.field_data(timestep, field, zone)
+        data = self.source.field_data(timestep, field, zone)
 
         # Coordinate conversion leaves scalar fields alone.
         if not field.is_geometry and not field.is_vector:
-            return indata
+            return data
 
         # For geometries, convert and store in cache.
         if field.is_geometry:
-            for a, b in util.pairwise(self.path):
-                self.cache[a.name, zone] = indata
-                indata = convert_coords(a, b, indata)
-            return indata
+            return self.cache.convert_geometry(data, zone)
 
         # For vectors, use cached data to convert.
-        for a, b in util.pairwise(self.path):
-            indata = convert_vectors(a, b, indata, self.cache[a.name, zone])
-
-        return indata
+        return self.cache.convert_vectors(data, zone)
